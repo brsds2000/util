6c84f817a782b90161a43fc778514b5c24f2c466
Bruno Rodrigo <bruno.rodrigo@animaeducacao.com>
2016-06-03 00:06:14 -0300

    acerto de arq de banco

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => 'util',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => env('DB_DATABASE','util'),
+			'username'  => env('DB_USERNAME','homestead'),
+			'password'  => env('DB_PASSWORD','secret'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
